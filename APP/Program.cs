2e79fa7a8bcc4149c0718ef6ec8717c42c029457
ZaaRam <zaa_ramirez@outlook.com>
2026-05-11 09:26:18 -0600

    UPDATE: Actualizacon program y correccion en pruebas

--- APP/Program.cs
+++ APP/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //Aplicacion de consola participante1
+//Crando codigo de clases etc ect etc etc etc
